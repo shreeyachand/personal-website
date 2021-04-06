c37c0a10683493408cc713bb38774fde02a9da4b
Shreeya Chand <chshreeya@gmail.com>
2021-04-06 14:12:26 -0400

    Footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
+import Col from 'react-bootstrap/Col';
+import './Footer.css';
 
 function Footer() {
 
     return(
         <footer className="mt-5">
-            <Container fluid={true}>
-                <Row className="border-top justify-content-between p-3">
-                </Row>
-            </Container>
+            <Col id="bottomText">
+                <Container fluid={true}>
+                    <Row className="border-top justify-content-between p-3">
+                    </Row>
+                    <Row>© 2021</Row>
+                    <Row><a href="https://shreeyachand.medium.com/">Medium</a></Row>
+                    <Row><a href="https://github.com/shreeyachand/">GitHub</a></Row>
+                    <Row><a href="mailto:hi@shreeyachand.me">hi@shreeyachand.me</a></Row>
+                </Container>
+            </Col>
         </footer>
     );
 
